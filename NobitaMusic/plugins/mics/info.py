4f12b223bca620a89d1d893897a6031f6b15b16c
iamnobita206243 <nobitamusic001@gmail.com>
2025-10-18 00:47:15 +0530

    Update info.py

--- NobitaMusic/plugins/mics/info.py
+++ NobitaMusic/plugins/mics/info.py
@@ -11,7 +11,7 @@ from typing import Union, Optional
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/ll_NOBITA_DEFAULTERS_ll?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/@NOBITA_MUSIC_ROBOT?startgroup=true"),
     ],
 ]
 
